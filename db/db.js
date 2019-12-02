46ceb266b3bca0c677865be97502b6ea8ee47fa7
Chris Hatch <starforgedcode@gmail.com>
2019-12-02 15:28:02 -0600

    JWT authentication.

---
+++ db/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const mongoURI = process.env.MONGODB_URL
+const options = {
+    user: process.env.MONGO_USER,
+    pass: process.env.MONGO_PASS,
+    keepAlive: true, 
+    keepAliveInitialDelay: 300000,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(mongoURI, options)
+    }
+    catch(e) {
+        console.error(e.message)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
